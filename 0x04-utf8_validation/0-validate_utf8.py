196b13df8daad19a253b060f3cd68fe31c7d6b56
Sonie <garachu.muthoni@gmail.com>
2024-05-23 14:11:57 +0300

    fix errors

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,37 +1,32 @@
+#!/usr/bin/python3
+"""UTF-8 Validation"""
+
+
+def get_leading_set_bits(num):
+    """returns the number of leading set bits (1)"""
+    set_bits = 0
+    helper = 1 << 7
+    while helper & num:
+        set_bits += 1
+        helper = helper >> 1
+    return set_bits
+
+
 def validUTF8(data):
-    # Number of bytes in the current UTF-8 character
-    num_bytes = 0
-
-    # Masks for checking the most significant bits
-    mask1 = 1 << 7  # 10000000
-    mask2 = 1 << 6  # 01000000
-
-    # Iterate over each integer in the data
-    for byte in data:
-        # Only look at the 8 least significant bits
-        byte = byte & 0xFF
-        
-        if num_bytes == 0:
-            # Determine the number of bytes in this UTF-8 character
-            mask = 1 << 7
-            while mask & byte:
-                num_bytes += 1
-                mask >>= 1
-
-            # If num_bytes is 0, it's a 1-byte character
-            if num_bytes == 0:
+    """determines if a given data set represents a valid UTF-8 encoding"""
+    bits_count = 0
+    for i in range(len(data)):
+        if bits_count == 0:
+            bits_count = get_leading_set_bits(data[i])
+            '''1-byte (format: 0xxxxxxx)'''
+            if bits_count == 0:
                 continue
-
-            # Characters should be between 2 and 4 bytes
-            if num_bytes == 1 or num_bytes > 4:
+            '''a character in UTF-8 can be 1 to 4 bytes long'''
+            if bits_count == 1 or bits_count > 4:
                 return False
         else:
-            # Check that the byte is a valid continuation byte
-            if not (byte & mask1 and not (byte & mask2)):
+            '''checks if current byte has format 10xxxxxx'''
+            if not (data[i] & (1 << 7) and not (data[i] & (1 << 6))):
                 return False
-
-        # Decrement the number of bytes to process
-        num_bytes -= 1
-
-    # If we finished processing all bytes, num_bytes should be 0
-    return num_bytes == 0
+        bits_count -= 1
+    return bits_count == 0
